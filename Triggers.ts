5c76b773861a3a60d52c963aaf3117537a02ad3b
Benji Altman <benji@altmansoftwaredesign.com>
2021-10-21 21:02:10 -0500

    ???

--- Triggers.ts
+++ Triggers.ts
@@ -1,10 +1,10 @@
 /// <reference path="Constants.ts"/>
 
 
-function onOpen()
-{
+function onOpen() {
 	SpreadsheetApp.getActive().addMenu("Chess", [
 		{ name: 'Add/Update Players', functionName: 'updatePlayers' },
+		{ name: 'Insert week\'s games', functionName: 'weeklyUpdate' },
 	]);
 	//var spreadsheet = SpreadsheetApp.getActive();
 	//var menuItems = [
@@ -16,64 +16,54 @@ function onOpen()
 
 
 /** Do update grades */
-function gradeUpdate()
-{
-	if(new Date().getMonth() === 6)
-	{
+function gradeUpdate() {
+	if (new Date().getMonth() === 6) {
 		let newGrade = (grade: string | number) => CONST.gradeAdvancement[grade] || <number>grade + 1;
 		let club = FrontEnd.getClub();
-		for(let name in club.Master)
+		for (let name in club.Master)
 			club.Master[name].grade = newGrade(club.Master[name].grade);
-		FrontEnd.setClub(club);
+		FrontEnd.setClub(club, true);
 	}
 }
 
 
-function weeklyUpdate()
-{
+function weeklyUpdate() {
 	WeeklyUpdate.doAll(true);
 }
 
 //TODO move namespace
 /** Namespace for everything that needs to be done to update the club after a week of games */
-namespace WeeklyUpdate
-{
+namespace WeeklyUpdate {
 	/**
 	 * Does all the stuff for a weekly update.
 	 * 
 	 * @param write should anything be written to the document, a truthy value will have the document written.
 	 */
-	export function doAll(write?: boolean)
-	{
+	export function doAll(write?: boolean) {
 		let attendance = FrontEnd.getAttendanceSheetData();
 		let gamesPlayed = FrontEnd.getGamesPlayedData();
 		let club = FrontEnd.getClub();
 
 		//if there were no games played this week
-		if(gamesPlayed.length === 0)
-		{
+		if (gamesPlayed.length === 0) {
 			//Only do glicko ratings and then end
 			let ratings = [];
-			for(var person in club.Master)
+			for (var person in club.Master)
 				ratings.push(club.Master[person].glicko);
 			Glicko.doRatingPeriod<string>(name => club.Master[name].glicko, [], ratings);
 		}
-		else
-		{
+		else {
 			//remove pairs that happened more than twice
 			let matchCount: { [matchString: string]: number } = {};
-			for(let i = 0; i < gamesPlayed.length; i++)
-			{
+			for (let i = 0; i < gamesPlayed.length; i++) {
 				let match = [gamesPlayed[i].white, gamesPlayed[i].black];
 				match = match.sort();
 				let matchStr = JSON.stringify(match);
-				if(matchCount[matchStr] >= CONST.maximumOfSameMatchPerWeek)
-				{
+				if (matchCount[matchStr] >= CONST.maximumOfSameMatchPerWeek) {
 					gamesPlayed.splice(i, 1);	//removes ith element
 					i--;
 				}
-				else
-				{
+				else {
 					matchCount[matchStr] = (matchCount[matchStr] || 0) + 1;
 				}
 			}
@@ -88,12 +78,11 @@ namespace WeeklyUpdate
 			consumeGamesPlayed(club, gamesPlayed, attendance);
 
 			//modify attendance based on who was here
-			for(let i = 0; i < club.Active.length; i++)
+			for (let i = 0; i < club.Active.length; i++)
 				club.Active[i].absent = !attendance[club.Active[i].name];
 		}
 
-		if(write)
-		{
+		if (write) {
 			FrontEnd.resetAttendancePage(club.Active);
 			FrontEnd.addGameLog({ games: gamesPlayed, attendance: attendance });
 			FrontEnd.resetGamesPlayedPage();
@@ -108,12 +97,10 @@ namespace WeeklyUpdate
 	 * @param club Active club portion
 	 * @param attendance attendance map (used as set)
 	 */
-	function tryRemoveLowestPlayer(club: IPlayer[], attendance: { [name: string]: boolean })
-	{
+	function tryRemoveLowestPlayer(club: IPlayer[], attendance: { [name: string]: boolean }) {
 		let lastPlace = club[club.length - 1];
 		//condition for removal
-		if(!lastPlace.registered && lastPlace.absent && !attendance[lastPlace.name])
-		{
+		if (!lastPlace.registered && lastPlace.absent && !attendance[lastPlace.name]) {
 			club.pop();
 			lastPlace.isActive = false;
 			return true;
@@ -128,10 +115,8 @@ namespace WeeklyUpdate
 	 * @param gamesPlayed The games played array
 	 * @returns a refrence to attenanceObj
 	 */
-	function adjustAttendance(attendanceObj: { [name: string]: boolean }, gamesPlayed: FrontEnd.IGamePlayed[])
-	{
-		for(let i = 0; i < gamesPlayed.length; i++)
-		{
+	function adjustAttendance(attendanceObj: { [name: string]: boolean }, gamesPlayed: FrontEnd.IGamePlayed[]) {
+		for (let i = 0; i < gamesPlayed.length; i++) {
 			attendanceObj[gamesPlayed[i].white] = true;
 			attendanceObj[gamesPlayed[i].black] = true;
 		}
@@ -150,11 +135,9 @@ namespace WeeklyUpdate
 	 * @param games array of all games played
 	 * @param attendanceObj attendance object, mapping player to if they were here today
 	 */
-	function consumeGamesPlayed(club: IClub, games: FrontEnd.IGamePlayed[], attendanceObj: { [name: string]: boolean })
-	{
+	function consumeGamesPlayed(club: IClub, games: FrontEnd.IGamePlayed[], attendanceObj: { [name: string]: boolean }) {
 		//adjusts games played for each player
-		for(let i = 0; i < games.length; i++)
-		{
+		for (let i = 0; i < games.length; i++) {
 			club.Master[games[i].white].gamesPlayed++;
 			club.Master[games[i].black].gamesPlayed++;
 		}
@@ -163,7 +146,7 @@ namespace WeeklyUpdate
 		Lampert.doRatingPeriod((name: string) => club.Master[name].lampert, games);
 		//Does all Glicko rating changes (using glicko 2 system)
 		let temp: Glicko.IRating[] = [];
-		for(let player in club.Master)
+		for (let player in club.Master)
 			temp.push(club.Master[player].glicko);
 		Glicko.doRatingPeriod((name: string) => club.Master[name].glicko, games, temp);
 
